b4d67e066e55351c5c8f0f8045bb1d90218606ba
slynn1324 <slynn1324@gmail.com>
2025-01-22 20:09:02 -0600

    cleanup run script

--- podman-run.sh
+++ podman-run.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-#HA_SYS_AGENT_MQTT_HOST=192.168.1.3 HA_SYS_AGENT_MQTT_USER=homeassistant HA_SYS_AGENT_MQTT_PASS=homeassistant HA_SYS_AGENT_NET_DEVS=eth0 python3 ha-sys-agent.py
-
 # use --net host to get access to read net stats for host network devices
 podman run -d \
     --pull newer \
@@ -12,5 +10,3 @@ podman run -d \
     -e HA_SYS_AGENT_NET_DEVS=eth0 \
     --net host \
     ghcr.io/slynn1324/ha-sys-agent:latest
-    
-    #ha-sys-agent
